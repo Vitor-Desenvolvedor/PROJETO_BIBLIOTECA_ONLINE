503a93b11930957c34b6919c596c6caad01a73e5
Vitor-Desenvolvedor <vitorcarvalho.014@gmail.com>
2023-08-15 21:52:23 -0300

    Atualização

--- listar.php
+++ listar.php
@@ -1,29 +1,48 @@
 <?php
+    // Incluir a pagina de conexão do banco de dados
+    //include 'conexao.php';
 
-          // Incluir a pagina de conexão do banco de dados
-          include 'conexao.php';
+    // Incluir conexão com o banco de dados - alterado para não aparecer a mensagem de "Database - OK"
+     //criacao de variaveis para conexao com o banco de dados
 
-            // Listar usuários cadastrados no sistema
+     $host = 'localhost';
+     $usuario = 'root';
+     $senha = '';
+     $banco = 'db_biblioteca_digital';
+ 
+     //Criacao de conexao com o banco de dados
+ 
+     $conexao = new mysqli($host,$usuario,$senha,$banco);
+ 
+     //teste de conexao com o banco de dados
+ 
+     if($conexao->connect_error){
+ 
+         die('Erro de conexao: '.$conexao->connect_error);
+ 
+     }   
 
-            $sqlListar = "SELECT * FROM tbl_livros";
-            $resultado = $conexao -> query($sqlListar);
+    // Listar usuários cadastrados no sistema
 
-            // Condicionamento para listar
-            if($resultado->num_rows > 0){
+    $sqlListar = "SELECT * FROM tbl_livros";
+    $resultado = $conexao -> query($sqlListar);
 
-                echo "<ul>";
-                while($linha = $resultado->fetch_assoc()){
-                
-                    echo "<li> Código:".$linha['codLivro']."| Título:".$linha['titulo']."| Autor:".$linha['autor']."| Gênero:".$linha['genero']."| Idioma:".$linha['idioma']." </li>";
+    // Condicionamento para listar
+    if($resultado->num_rows > 0){
 
-                }
+        echo "<ul>";
+        while($linha = $resultado->fetch_assoc()){
+        
+            echo "<li> Código:".$linha['codLivro']."| Título:".$linha['titulo']."| Autor:".$linha['autor']."| Gênero:".$linha['genero']."| Idioma:".$linha['idioma']." </li>";
 
-                echo "</ul>";
+        }
 
-            } else {
+        echo "</ul>";
 
-                echo "Nenhum valor encontrado!";
+    } else {
 
-            }
+        echo "Nenhum valor encontrado!";
+
+    }
 
 ?>
