b5e4a06d277d116d37b2b0ab3ab1a5be6e5fb3da
Vitor-Desenvolvedor <vitorcarvalho.014@gmail.com>
2023-08-23 18:57:34 -0300

    Create autenticar.php

---
+++ autenticar.php
@@ -0,0 +1,19 @@
+<?php
+
+    $usuariovalido = "teste";
+    $senhavalida = "123";
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+        $usuarioInput = $_POST['usuario'];
+        $senhaInput = $_POST['senha'];
+
+        if($usuarioInput === $usuariovalido && $senhaInput === $senhavalida){
+
+            $_SESSION['autenticado'] = TRUE;
+            header("location: principal.php");
+        }
+    }
+
+
+?>
