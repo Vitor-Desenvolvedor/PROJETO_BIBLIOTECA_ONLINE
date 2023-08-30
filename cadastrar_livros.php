009bfc6328cef1eccdbfd46f9c8225a828a5e5c9
Vitor-Desenvolvedor <vitorcarvalho.014@gmail.com>
2023-08-30 20:18:49 -0300

    Update cadastrar_livros.php
    * Adição do sub menu.

--- cadastrar_livros.php
+++ cadastrar_livros.php
@@ -24,6 +24,21 @@
                 <li><a href="cadastrar_livros.php">Cadastrar</a></li>
                 <li><a href="#Relatorios">Relatórios</a></li>
             </ul>
+            <div class="usuarios">
+                <div>
+                <a href="#Usuários">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
+                    </svg>Usuários</a>
+                </div>
+                <div class="sub-usuarios">
+                    <ul>
+                        <li><a href="#Perfil" class="botão-perfil">Perfil</a></li>
+                        <li><a href="#NovaSenha" class="botão-alterar-senha">Alterar Senha</a></li>
+                        <li><a href="cadastrar_usuario.php" class="botão-novo-usuario">Novo Usuário</a></li>
+                        <li><a href="logout.php" class="botão-logout">Sair</a></li>
+                    </ul>
+                </div>
         </nav>
     </header>
     <main class="card-principal">
