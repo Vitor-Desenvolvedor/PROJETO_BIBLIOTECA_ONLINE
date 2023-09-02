424775b9f52acde79bca08171377504bad03355a
Vitor-Desenvolvedor <vitorcarvalho.014@gmail.com>
2023-09-01 21:46:27 -0300

    Update listar.php

--- listar.php
+++ listar.php
@@ -52,4 +52,24 @@
 
     }
 
+    //Consulta SQL para excluir registro 
+    
+    if(isset($_POST['id_excluir'])){
+        $idExcluir = $_POST["id_excluir"];
+        echo $idExcluir;
+        $codigolivro = $_POST["codLivro"];
+        //echo "SELECT FROM tbl_livros WHERE codLivro=$codigolivro";
+        $sqlExcluir = "DELETE FROM tbl_livros WHERE codLivro = '$codigolivro'";
+
+
+        if ($conexao->query($sqlExcluir) === TRUE) {
+
+            echo "<br>","Registro Excluido com sucesso!";
+            header ("location: pesquisar_livros.php");
+        } else {
+            header ("location: pesquisar_livros.php");
+            echo "<br>","Erro ao excluir o registro".$conexao->error;
+        }
+    }
+
 ?>
