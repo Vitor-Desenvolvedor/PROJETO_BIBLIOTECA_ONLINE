e2495efd6836587a87dfdb9a1b45ee94e0f59ca9
Vitor-Desenvolvedor <vitorcarvalho.014@gmail.com>
2023-09-01 21:46:36 -0300

    Update excluir.php

--- excluir.php
+++ excluir.php
@@ -1,4 +1,4 @@
-<?php
+<?php /*
 
 // Inclui página de conexão com o banco de dados
 include 'conexao.php';
@@ -9,8 +9,9 @@ include 'conexao.php';
     if(isset($_POST['id_excluir'])){
         $idExcluir = $_POST["id_excluir"];
         echo $idExcluir;
+        $codigolivro = $_POST["codLivro"];
         //echo "SELECT FROM tbl_livros WHERE codLivro=$codigolivro";
-        $sqlExcluir = "DELETE FROM tbl_livros WHERE codLivro = $idExcluir";
+        $sqlExcluir = "DELETE FROM tbl_livros WHERE codLivro = '$codigolivro'";
 
 
         if ($conexao->query($sqlExcluir) === TRUE) {
@@ -21,5 +22,5 @@ include 'conexao.php';
             header ("location: pesquisar_livros.php");
             echo "<br>","Erro ao excluir o registro".$conexao->error;
         }
-    }
+    }*/
 ?>
